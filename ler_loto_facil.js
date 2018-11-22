f1a984698f2ed73c1b9019f9cea705ce3e7de4d3
Daniel Oliveira <danielso2007@gmail.com>
2018-11-22 11:49:07 -0200

    feat: Ordenando números e verificando repetições.

--- ler_loto_facil.js
+++ ler_loto_facil.js
@@ -1,11 +1,28 @@
 var lineReader = require('line-reader'), Promise = require('bluebird');
 
 var numeroMap = new Map();
+var jogosMap = new Map();
 var jogos = [];
+var repetidos = 0;
+var naoRepetidos = 0;
 
 let lerLinha = (line) => {
-    jogos.push(line);
+    let arrayNumerosSort = line.split(",").sort((a,b) => {
+      return Number(a) > Number(b) ? 1 : -1;
+    });
+
+    jogos.push(arrayNumerosSort.toString());
+
+    if (jogosMap.get(arrayNumerosSort.toString())) {
+      jogosMap.set(arrayNumerosSort.toString(), jogosMap.get(arrayNumerosSort.toString()) + 1);
+      repetidos++;
+    } else {
+      jogosMap.set(arrayNumerosSort.toString(), 1);
+      naoRepetidos++;
+    }
+
     let arrayNumeros = line.split(",");
+
     arrayNumeros.forEach(element => {
         let num = Number(element);
         let numero = numeroMap.get(num);
@@ -46,6 +63,9 @@ eachLine('./numeros.csv', function(line) {
 }).then(function() {
   var mapAsc = new Map([...numeroMap.entries()].sort((a, b) => a[1] > b[1] ? -1 : 1));
 
+  console.log('Não repetidos: ', naoRepetidos);
+  console.log('Repetidos: ', repetidos);
+
   let listaChaves = [];
   for (var key of mapAsc.keys()) {
     listaChaves.push(key);
@@ -61,17 +81,22 @@ eachLine('./numeros.csv', function(line) {
   for (let x = 0; x < 15; x++) {
     _15PrimeirosNumeros.push(listaChaves[x]);
   }
-  _15PrimeirosNumeros.sort((a,b) => a > b ? 1 : -1);
 
   let _15UltimosNumeros = [];
   for (let x = (listaChaves.length - 15); x < listaChaves.length; x++) {
     _15UltimosNumeros.push(listaChaves[x]);
   }
-  _15UltimosNumeros.sort((a,b) => a > b ? 1 : -1);
 
   console.log('Todos os números: ', _15MaisVotados.toString());
   console.log('Primeiros números: ', _15PrimeirosNumeros.toString());
   console.log('Últimos números  : ', _15UltimosNumeros.toString());
+
+  let jogo01 = '1,2,3,4,5,7,8,9,10,11,13,15,18,21,24';
+  let jogo02 = '2,3,4,6,7,9,12,14,15,17,20,21,23,24,25';
+  let jogo03 = '1,2,3,4,6,7,8,9,10,11,13,14,17,21,23';
+
+  _15PrimeirosNumeros.sort((a,b) => a > b ? 1 : -1);
+  _15UltimosNumeros.sort((a,b) => a > b ? 1 : -1);
 
   for ((value) of jogos) {
     if (value === _15PrimeirosNumeros.toString()) {
@@ -79,6 +104,15 @@ eachLine('./numeros.csv', function(line) {
     }
     if (value === _15UltimosNumeros.toString()) {
       console.log(`Jogo ${_15PrimeirosNumeros.toString()} já existe`);
+    }
+    if (value === jogo01) {
+      console.log(`Jogo ${jogo01} já existe`);
+    }
+    if (value === jogo02) {
+      console.log(`Jogo ${jogo02} já existe`);
+    }
+    if (value === jogo03) {
+      console.log(`Jogo ${jogo03} já existe`);
     }
   }
 
